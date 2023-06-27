c7a69fb96b412b60243a89d8bee4557f30606c49
Justin Lau <jlau@bcsautomation.ca>
2023-06-27 15:01:43 -0400

    Add project files.

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.siticoneAnimateWindow1 = new Siticone.Desktop.UI.WinForms.SiticoneAnimateWindow(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TrackerBar = new Siticone.Desktop.UI.WinForms.SiticoneTrackBar();
+            this.PowerButton = new System.Windows.Forms.Button();
+            this.CPSVAL = new System.Windows.Forms.Label();
+            this.Autoclicker = new System.Windows.Forms.Timer(this.components);
+            this.Random = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "8CLICKER";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(189, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TrackerBar
+            // 
+            this.TrackerBar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.TrackerBar.Location = new System.Drawing.Point(28, 236);
+            this.TrackerBar.Maximum = 25;
+            this.TrackerBar.Minimum = 1;
+            this.TrackerBar.Name = "TrackerBar";
+            this.TrackerBar.Size = new System.Drawing.Size(300, 23);
+            this.TrackerBar.TabIndex = 2;
+            this.TrackerBar.ThumbColor = System.Drawing.Color.DarkGreen;
+            this.TrackerBar.Value = 10;
+            this.TrackerBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.siticoneTrackBar1_Scroll);
+            // 
+            // PowerButton
+            // 
+            this.PowerButton.BackColor = System.Drawing.Color.Green;
+            this.PowerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PowerButton.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.PowerButton.Location = new System.Drawing.Point(98, 82);
+            this.PowerButton.Name = "PowerButton";
+            this.PowerButton.Size = new System.Drawing.Size(75, 68);
+            this.PowerButton.TabIndex = 3;
+            this.PowerButton.Text = "ON";
+            this.PowerButton.UseVisualStyleBackColor = false;
+            this.PowerButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CPSVAL
+            // 
+            this.CPSVAL.AutoSize = true;
+            this.CPSVAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPSVAL.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CPSVAL.Location = new System.Drawing.Point(178, 80);
+            this.CPSVAL.Name = "CPSVAL";
+            this.CPSVAL.Size = new System.Drawing.Size(106, 73);
+            this.CPSVAL.TabIndex = 4;
+            this.CPSVAL.Text = "10";
+            this.CPSVAL.Click += new System.EventHandler(this.CPSVAL_Click);
+            // 
+            // Autoclicker
+            // 
+            this.Autoclicker.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Random
+            // 
+            this.Random.Tick += new System.EventHandler(this.Random_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(349, 361);
+            this.Controls.Add(this.CPSVAL);
+            this.Controls.Add(this.PowerButton);
+            this.Controls.Add(this.TrackerBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "AutoClicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticoneAnimateWindow siticoneAnimateWindow1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Siticone.Desktop.UI.WinForms.SiticoneTrackBar TrackerBar;
+        private System.Windows.Forms.Button PowerButton;
+        private System.Windows.Forms.Label CPSVAL;
+        private System.Windows.Forms.Timer Autoclicker;
+        private System.Windows.Forms.Timer Random;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+
+    public partial class Form1 : Form
+    {
+        //local vars 
+        int min;
+        int max;
+        int randVal;
+        Random rand = new Random();
+
+        //DLLs required for click access
+
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("User32.dll")]
+
+        //Main code
+
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            CPSVAL.Text = TrackerBar.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (PowerButton.Text.Contains("ON"))
+            {
+                PowerButton.Text = "OFF";
+                Autoclicker.Start();
+            }
+            else if (PowerButton.Text.Contains("OFF"))
+            {
+                PowerButton.Text = "ON";
+                Autoclicker.Stop();
+            }
+        }
+
+        private void Random_Tick(object sender, EventArgs e)
+        {
+            if (PowerButton.Text.Contains("OFF"))
+            {
+                min = Math.Max(1, TrackerBar.Value - 6); // Enforces min greater than one
+                max = TrackerBar.Value + 6; // Enforces max greater than one
+                randVal = rand.Next(min, max);
+            }
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (randVal > 0) // Added this to avoid division by zero errors
+                {
+                    Autoclicker.Interval = 1000 / randVal;
+                    CPSVAL.Text = randVal.ToString();
+                }
+            }
+            catch
+            {
+                Console.WriteLine(e);
+            }
+
+            if (PowerButton.Text.Contains("OFF") && MouseButtons == MouseButtons.Left)
+            {
+                IntPtr hWnd = GetForegroundWindow(); // Gets current window, but can be replaced with specific control or window
+                const int WM_LBUTTONDOWN = 0x0201;
+                const int WM_LBUTTONUP = 0x0202;
+                PostMessage(hWnd, WM_LBUTTONDOWN, 0, 0);
+                await Task.Delay(30);
+                PostMessage(hWnd, WM_LBUTTONUP, 0, 0); // This is the message for mouse left button up    
+            }
+        }
+
+        private void CPSVAL_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
